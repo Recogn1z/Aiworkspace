b80c72797b3626113d9f84b365fa30013ba586e9
Recogn1z <543257239@qq.com>
2026-05-19 04:11:37 -0700

    add register router

--- router/router.go
+++ router/router.go
@@ -11,6 +11,11 @@ func SetupRouter() *gin.Engine {
 	api := r.Group("/api/v1")
 	{
 		api.GET("/health", controller.HealthCheck)
+
+		auth := api.Group("/auth")
+		{
+			auth.POST("/register",controller.Register)
+		}
 	}
 	return r
 }
